53a55be967c6d53e4bd62b8069a51eb398fff5a1
Javier Castillo Calvo <jcastillo@xuitec.com>
2017-10-26 00:11:15 +0200

    Funcionando primera vez

--- ServerWeb.go
+++ ServerWeb.go
@@ -1,1 +1,34 @@
-"" 
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func funcGet(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, "valor: %s", params["valor"])
+}
+
+func main() {
+	fmt.Println("Inicio del programa")
+	MiMux := mux.NewRouter().StrictSlash(true)
+	MiMux.HandleFunc("/api/{valor}", funcGet).Methods("GET")
+	MiMux.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
+	MiServer := &http.Server{
+		Addr:           ":5050",
+		Handler:        MiMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatalln(MiServer.ListenAndServe())
+}
